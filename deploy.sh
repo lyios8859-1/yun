dbfccbff7ebafdeb9bbdad65ce3f6ccebfc1a37c
lyios8859-1 <194962218@qq.com>
2022-07-25 08:22:55 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,15 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
 npm run build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+mv docs/.vitepress/dist .
+
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -23,4 +22,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 git push -f https://github.com/lyios8859-1/yun.git main:gh-pages
 
-# cd -
+cd -
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,46 +1,7 @@
-// module.exports = {
-//   title: "YUN-UI",
-//   description: "Vue3 YUN UI",
-//   themeConfig: {
-//     logo: "/logo.svg",
-
-//     socialLinks: [
-//       { icon: "github", link: "https://github.com/vuejs/vitepress" },
-//     ],
-
-//     nav: [
-//       { text: "指南", link: "/guide/installation", activeMatch: "/guide/" },
-//       { text: "组件", link: "/component/icon", activeMatch: "/component/" },
-//     ],
-
-//     sidebar: {
-//       "/guide/": [
-//         {
-//           text: "指南",
-//           items: [
-//             { text: "安装", link: "/guide/installation" },
-//             { text: "快速开始", link: "/guide/quickStart" },
-//           ],
-//         },
-//       ],
-//       "/component/": [
-//         {
-//           text: "基础组件",
-//           items: [{ text: "Icon", link: "/component/icon" }],
-//         },
-//       ],
-//     },
-
-//     footer: {
-//       message: "Released under the MIT License.",
-//       copyright: "Copyright © 2022-present Timly",
-//     },
-//   },
-// };
-
 module.exports = {
   title: "YUN-UI",
   description: "Vue3 YUN UI",
+  base: "/yun/",
   themeConfig: {
     logo: "/logo.svg",
     socialLinks: [
